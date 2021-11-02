bd7c5d82507e9263451258b66b0ff8c737156d5b
johnhitz203 <johnhitz203@gmail.com>
2021-11-02 13:14:45 -0500

    Created playRound function

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -9,4 +9,55 @@ function computerPlay() {
   return possibleResponses[guess];
 }
 
+// Play Round function //
+// takes two parameters (playerSelection, computerSelection)
+// if playerSelection is 'rock' and computerSelection is scissors player wins
+// if playerSelection is 'rock' and computerSelection is paper player loses
+// if playerSelection is 'rock' and computerSelection is 'rock' it's a tie
+/////////////////////////////////////////////////////
+// playerSelection | computerSelection | Winner    //
+// ----------------|-------------------|---------- //
+//       Same      |      Same         | tie       //
+// ----------------------------------------------- //
+//       Rock      |      Paper        | computer  //
+// ----------------|-------------------|---------- //
+//       Paper     |      Scissors     | computer  //
+// ----------------|-------------------|---------- //
+//       Scissors  |      Rock         | computer  //
+// ----------------------------------------------- //
+//       Paper     |      Rock         | player    //
+// ----------------------------------------------- //
+//       Rock      |      Scissors     | player    //
+// ----------------------------------------------- //
+//       Scissors  |      Paper        | player    //
+////////////////////////////////////////////////////
 //
+// return string that declares the winner - "You Lose! Paper beats Rock."
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection === computerSelection) {
+    return 'Tie';
+  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+    return 'Paper covers rock. Computer wins!';
+  } else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+    return 'Scissors cuts paper. Computer wins!';
+  } else if(playerSelection === 'Scissors' && computerSelection === 'Rock') {
+    return 'Rock breaks scissors. Computer wins';
+  } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+    return 'Paper covers rock. Player wins!';
+  } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+    return 'Rock breaks scissors. Player wins!';
+  } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+    return 'Scissors cuts paper. Player wins!'
+  } else return 'Please select from one of Rock, Paper, or Scissors'
+}
+
+// game fucntion //
+// create variable array with score for computer and player
+// loop 5 times
+//// prompt player for selection
+//// playRound
+//// report after each round who won
+//// increment winner score be 1
+
+function game()
+// let PlayerSelection = window.prompt("Make a guess (Rock, Paper, Scissors)");
